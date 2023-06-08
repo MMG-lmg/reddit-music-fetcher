36d2c8debbfa1d1e9d52f9a12f9fb7e3009d74c2
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2023-06-08 19:02:31 +0200

    feat: implemented date conversion

--- v0/object_reformater.py
+++ v0/object_reformater.py
@@ -1,6 +1,6 @@
 import json_io
 import processed_types
-
+from datetime import datetime
 post_data = json_io.load_data('post_data')
 comment_data = json_io.load_data('comment_data')
 user_data = json_io.load_data('user_data')
@@ -9,11 +9,15 @@ post_user_data=json_io.load_data('post_author_data')
 print('Building dictionary for faster access on comment authors') 
 post_user_dict = {} 
 for item in post_user_data:
+    if item['is_suspended'] != True: 
+        item["created_utc"] = datetime.utcfromtimestamp(int(item["created_utc"])).strftime('%Y-%m-%d %H:%M:%S')
     post_user_dict[item["name"]] = item
     
 print('Building dictionary for faster access on post authors') 
 comment_user_dict = {} 
 for item in user_data:
+    if item['is_suspended'] != True: 
+        item["created_utc"] = datetime.utcfromtimestamp(int(item["created_utc"])).strftime('%Y-%m-%d %H:%M:%S')
     comment_user_dict[item["name"]] = item
     
 post_dict = {}  
@@ -35,7 +39,8 @@ for count in range(1,len(post_data)):
         author = processed_types.User()
         author.name = '[deleted]'
         post["author"] = author
-    #build an dictionary for faster access  
+    #build an dictionary for faster access
+    post["created_utc"] = datetime.utcfromtimestamp(int(post["created_utc"])).strftime('%Y-%m-%d %H:%M:%S')
     post_dict[post['id']] = post
     post_dict[post['id']]['comments'] = []
       
@@ -57,27 +62,10 @@ for count in range(1,len(comment_data)):
         author.name = '[deleted]'
         post["author"] = author
     #add to dictionary
+    comment["created_utc"] = datetime.utcfromtimestamp(int(comment["created_utc"])).strftime('%Y-%m-%d %H:%M:%S')
     post_id = comment['parent_id'].split('_')[1] #post_id
     post_dict[post_id]['comments'].append(comment)
- 
-"""       
-#build an dictionary for faster access
-print('Building dictionary for faster access on posts')   
-post_dict = {}    
-for count in range(1,len(post_data)):
-    post = post_data[count-1]
-    print(f"Processed {count} of {len(post_data)} = {str(round(count/len(post_data)*100,2))}%")
-    post_dict[post['id']] = post
-    post_dict[post['id']]['comments'] = []
 
-print('Processing post comments')  
-for count in range(1,len(comment_data)):
-    comment = comment_data[count-1]
-    print(f"Processed {count} of {len(comment_data)} = {str(round(count/len(comment_data)*100,2))}%")
-    post_id = comment['parent_id'].split('_')[1] #post_id
-    post_dict[post_id]['comments'].append(comment)
-""" 
- 
 json_ready_list = []
 json_ready_list.extend(post_dict.values())
 json_io.store_data(json_ready_list,'reformated_posts')
